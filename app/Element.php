b5c6a2bfc422f94399af2145c7a2450f9ddc2b59
yane <yane.lisset@academico.ifpb.edu.br>
2020-02-21 17:32:17 -0300

    criacao do modelElement e ElementsControllers

---
+++ app/Element.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Element extends Model
+{
+    protected $table = "chemical_element";
+    public $timestamps = false;
+    protected $fillable = ['name', 'symbol', 'weight'];
+
+    public static function findOrAbort($id){
+        if (!$element = Element::find($id)) {
+            $element = [
+                'error' => [
+                    'code' => 'ERR-NOTFOUND',
+                    'http_code' => '404',
+                    'message' => 'Requested element cannot be found in the database.',
+                ]
+            ];
+            return \Response::json($error, 404);
+        }
+        return $element;
+    }
+}
